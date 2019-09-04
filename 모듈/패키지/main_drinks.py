45a84883890f88e3dbf19422f17c9799bad0c413
wjdals825 <s2018w30@e-mirim.hs.kr>
2019-09-05 08:59:43 +0900

    foods.drinks 활용

---
+++ 모듈/패키지/main_drinks.py
@@ -0,0 +1,8 @@
+from foods.drinks import *
+#from foods.drinks import milk, cola, water
+#from foods.drinks import cola
+#from foods.drinks import water
+
+milk.drink()
+cola.drink()            #톡톡 캬아~ 역시 콜라는 정민이네 콜라
+water.drink()           #맑고 투명한 물은 역시 지하수
