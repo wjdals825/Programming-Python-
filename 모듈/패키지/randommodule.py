74fef66358119b5f8e67188066f13a01b629b64f
wjdals825 <s2018w30@e-mirim.hs.kr>
2019-09-09 09:34:07 +0900

    add randommodule

---
+++ 모듈/패키지/randommodule.py
@@ -0,0 +1,35 @@
+import random
+
+print('0이상 1미만 실수 값')
+print('random.random() : ', random.random())
+print()
+print('시작값 2.5 이상 끝값 10.0 미만 실수 값')
+print('random.random(2.5, 10.0) : ', random.uniform(2.5, 10.0))
+print()
+print('0이상 끝값 10미만 정수 값')
+print('random.randrange(10) : ', random.randrange(10))
+print()
+print('1이상 끝값 7 미만, 증가 값이 2 인 정수 값')
+print('random.randrange(1,7,2) : ', random.randrange(1,7,2))
+print()
+print('1이상 끝값 7 이하, 정수 값')
+print('random.randint(1,7) : ', random.randint(1, 7))
+# random.randrange(1, 7+1)
+print()
+
+print('리스트에서 1개 값 꺼내오기')
+season = ['봄', '여름', '가을', '겨울']
+print('season : ', season)
+print('random.choice(season) : ', random.choice(season))
+print()
+l = ['가', '나', '다', '라', '마']
+print('리스트 순서를 섞기')
+print('섞기 전 ㅣ = ', l)
+random.shuffle(l)
+print('random.shuffle(l)')
+print('섞은 후 l = ', l)
+print()
+print('리스트에서 몇 개의 값을 중복하지 않고 3개 뽑기')
+sample = ['1번', '2번', '3번', '4번', '5번', '6번', '7번', '8번', '9번']
+print('샘플 대상 : ', sample)
+print('random.sample(sample,3) : ', random.sample(sample,3))
