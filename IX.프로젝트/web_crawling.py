3fe068c521642a0ede2d3bc9e4b17a369129727b
wjdals825 <s2018w30@e-mirim.hs.kr>
2019-11-25 09:35:44 +0900

    web Crawling - 연애혁명 제목,링크 가져오자

--- IX.프로젝트/web_crawling.py
+++ IX.프로젝트/web_crawling.py
@@ -7,9 +7,12 @@ from urllib.request import urlopen
 if __name__=='__main__':
     #네이버웹툰> 연애형멱 제목 가져오자
     data = urlopen("https://comic.naver.com/webtoon/list.nhn?titleId=570503&weekday=thu")
-    soup = BeautifulSoup(data, "lxml")
+    soup = BeautifulSoup(data, "lxml")          #httpResponse -> HTML
     # print(soup)
-    cartoon_titles = soup.find_all("td", attrs={"class":"title"})
-    for cartoon_title in cartoon_titles:
-        title = cartoon_title.find("a").text
+    cartoon_titles = soup.find_all("td", attrs={"class":"title"})    #<td class="title">...</td>
+    for cartoon_title in cartoon_titles:                               #cartoon_titles[:2]
+        title = cartoon_title.find("a").text                           #텍스트 가져오기.<a>text</a>
+        link = cartoon_title.find("a").get("href")                    #태그의 속성값 가져오기, <a href="">text</a>
+        link = "https://naver.com" + link
         print(title)
+        print(link)
