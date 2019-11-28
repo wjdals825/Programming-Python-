5d0c13d7da84b2b664e0315eb40dade0d53a2291
wjdals825 <s2018w30@e-mirim.hs.kr>
2019-11-28 10:03:07 +0900

    web Crawling - 다음웹툰 취향저격 그녀 json, html

--- IX.프로젝트/web_crawling_daumwebtoon.py
+++ IX.프로젝트/web_crawling_daumwebtoon.py
@@ -3,6 +3,7 @@
 #pycharm > File > Settings... > Project:... > ProjectInterPerter > + > beautifulsoup4 > install
 #pycharm > File > Settings... > Project:... > ProjectInterPerter > + > lxml > install
 
+from bs4 import BeautifulSoup
 from urllib.request import urlopen
 import json
 
@@ -10,13 +11,21 @@ if __name__ == '__main__':
     #네이버웹툰 > 연애혁명 제목 가져오자
     with urlopen("http://webtoon.daum.net/data/pc/webtoon/view/favorite") as data:
         j = json.loads(data.read())
-    print(j["data"]["webtoon"]["webtoonEpisodes"][3]["title"])
+    # print(j["data"]["webtoon"]["webtoonEpisodes"][3]["title"])
+    html = "<html><head><meta charset='utf-8'></head><body>"
     cartoon_titles = j["data"]["webtoon"]["webtoonEpisodes"]
     for cartoon_title in cartoon_titles:
         title = cartoon_title["title"]
-        print(title)
+        # print(title)
         thumbnail = cartoon_title["thumbnailImage"]["url"]
-        print(thumbnail)
+        # print(thumbnail)
         url = cartoon_title["id"]
         url = "http://webtoon.daum.net/webtoon/viewer/"+ str(url)
-        print(url)
+        # print(url)
+        html += "<a href='{}'><img src='{}'/>{}</a>".format(url,thumbnail,title)
+    html += "</body></html>"
+
+    outputSoup = BeautifulSoup(html, "lxml")  # 내가 생성한 html 문자열을 soup 객체로 만들자
+    prettyHtml = str(outputSoup.prettify())  # 예쁘게 html코드로 만들자
+    with open("취향저격 그녀.html", "w", encoding="utf-8") as f:  # html파일 만들자
+        f.write(prettyHtml)
