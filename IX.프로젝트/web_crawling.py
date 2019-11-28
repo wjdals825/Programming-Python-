6a8edbe5b31dcc885745284a396d2ca08d927fc9
wjdals825 <s2018w30@e-mirim.hs.kr>
2019-11-28 09:50:26 +0900

    web crowling - 네이버 웹툰

--- IX.프로젝트/web_crawling.py
+++ IX.프로젝트/web_crawling.py
@@ -1,26 +1,30 @@
 #pip install beautifulsoup4
 #pip install lxml
+#pycharm > File > Settings... > Project:... > ProjectInterPerter > + > beautifulsoup4 > install
+#pycharm > File > Settings... > Project:... > ProjectInterPerter > + > lxml > install
 
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 
-if __name__=='__main__':
-    #네이버웹툰> 연애형멱 제목 가져오자
-    data = urlopen("https://comic.naver.com/webtoon/list.nhn?titleId=570503&weekday=thu")
-    soup = BeautifulSoup(data, "lxml")          #httpResponse -> HTML
+if __name__ == '__main__':
+    #네이버웹툰 > 연애혁명 제목 가져오자
+    data = urlopen("https://comic.naver.com/webtoon/list.nhn?titleId=570503")
+    soup = BeautifulSoup(data,"lxml") #httpResponse -> HTML
+
     # print(soup)
     html = "<html><head><meta charset='utf-8'></head><body>"
-    cartoon_titles = soup.find_all("td", attrs={"class":"title"})    #<td class="title">...</td>
-    for cartoon_title in cartoon_titles:                               #cartoon_titles[:2]
-        title = cartoon_title.find("a").text                           #텍스트 가져오기.<a>text</a>
-        link = cartoon_title.find("a").get("href")                    #태그의 속성값 가져오기, <a href="">text</a>
-        link = "https://naver.com" + link
-        # print(title)
-        # print(link)
-        html +="<a href='{}'>{}</a><br />".format(link, title)            #<a href='link'>title</a>
+    cartoon_titles = soup.find_all("td", attrs={"class":"title"}) #<td class="title">...</td>
+    for cartoon_title in cartoon_titles:                          #cartoon_titles[:2]
+        title = cartoon_title.find("a").text                      #<a>text</a>
+        link = cartoon_title.find("a").get("href")                #태그의 속성값 가져오기, <a href="">text</a?
+        link="https://comic.naver.com" + link
+        print(title)
+        print(link)
+        html+="<a href='{}'>{}</a><br/>".format(link,title)
     html += "</body></html>"
-    # print(html)
-    outputSoup = BeautifulSoup(html, "lxml")
-    prettyHtml = str(outputSoup.prettify())
-    with open("연애혁명.html","w", encoding="utf-8") as f:
+    #print(hmtl)
+    outputSoup=BeautifulSoup(html, "lxml")  #내가 생성한 html 문자열을 soup 객체로 만들자
+    prettyHtml = str(outputSoup.prettify()) #예쁘게 html코드로 만들자
+    with open("연애혁명.html","w",encoding="utf-8") as f: #html파일 만들자
         f.write(prettyHtml)
+
