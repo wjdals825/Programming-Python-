ce1146f1e0440425ce5413782bb03cf7bcd61b3d
wjdals825 <s2018w30@e-mirim.hs.kr>
2019-12-05 10:10:58 +0900

    pyautogui

---
+++ IX.프로젝트/macro1.py
@@ -0,0 +1,11 @@
+import pyautogui as pag
+
+if __name__ == '__main__':
+    pag.moveTo(144, 473, duration=2)
+    # pag.doubleclick()
+    # pag.click()
+    # pag.click()
+    # pag.click(clicks=2)
+    pag.move(100, 200, duration=1)
+    # pag.rightClick()
+    pag.click(button="right")
---
+++ IX.프로젝트/mouse.py
@@ -0,0 +1,8 @@
+import pyautogui as pag
+
+if __name__ == '__main__':
+    while True:
+        x, y = pag.position()
+        print(x, y)
+
+        #144 473
